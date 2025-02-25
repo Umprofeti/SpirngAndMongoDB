028081a693975dcac2bb8e1acbd55e1ca1c975ee
Umprofeti <vansthewall16@gmail.com>
2025-02-25 13:58:20 -0500

    Inital commit

---
+++ src/main/java/com/skiatel/spingmongodb/SpingMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.skiatel.spingmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingMongoDbApplication.class, args);
+    }
+
+}
