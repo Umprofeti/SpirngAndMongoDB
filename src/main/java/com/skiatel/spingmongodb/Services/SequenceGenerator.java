fbda36531823bca402af9280d9947a18705f4449
Umprofeti <vansthewall16@gmail.com>
2025-02-25 16:07:44 -0500

    Fix: Se actualiza la importación

--- src/main/java/com/skiatel/spingmongodb/Services/SequenceGenerator.java
+++ src/main/java/com/skiatel/spingmongodb/Services/SequenceGenerator.java
@@ -24,9 +24,9 @@ public class SequenceGenerator {
     }
 
     public long generateSequence(String seqName) {
-        net.guides.springboot.crud.model.DatabaseSecuence counter = mongoOperations.findAndModify(query(where("_id").is(seqName)),
+        com.skiatel.spingmongodb.model.DatabaseSecuence counter = mongoOperations.findAndModify(query(where("_id").is(seqName)),
                 new Update().inc("seq",1), options().returnNew(true).upsert(true),
-                net.guides.springboot.crud.model.DatabaseSecuence.class);
+                com.skiatel.spingmongodb.model.DatabaseSecuence.class);
         return !Objects.isNull(counter) ? counter.getSeq() : 1;
     }
 }
